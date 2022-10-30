5b7e3340a45d92d72f580ae6e29612160d9953e0
Eye <47392011+vtheskeleton@users.noreply.github.com>
2022-10-30 17:28:10 +0000

    Basic token system and utility methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello world!")
+import express, {Request, Response} from 'express';
+import db from "./db";
+const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
+
+app.use(express.json());
+
+import auth from './routes/auth';
+app.use("/v1/auth", auth);
+
+app.use("*", (req : Request, res : Response) => {
+    res.status(403).end();
+})
+
+let port = process.env.EMS_API_PORT || 1337
+db.dbEvents.on("ready", () => {
+    app.listen(port, () => {
+        console.info(`App listening on ${port}`);
+    })
+})
